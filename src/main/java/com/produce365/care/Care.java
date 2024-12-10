17cb1a4f3c7d4da9ce51a034b133127446b4509e
박호영 <hoyoung@baghoyeong-ui-MacBookAir.local>
2024-12-10 16:00:39 +0900

    [FEAT] : Table 어너테이션 기능 추가

--- src/main/java/com/produce365/care/Care.java
+++ src/main/java/com/produce365/care/Care.java
@@ -1,7 +1,19 @@
 package com.produce365.care;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@Entity
+@Table(name = "CARE")
 public class Care {
+	@Id
 	private int id;
+	
 	private String category;
+	
 	private int cost;
 }
