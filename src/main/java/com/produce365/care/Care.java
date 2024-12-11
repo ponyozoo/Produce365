a701347dd460444729a0818de79fe42ef5e5fbcd
박호영 <hoyoung@baghoyeong-ui-MacBookAir.local>
2024-12-11 10:28:37 +0900

    [FEAT] : CareHistory 클래스 생성

--- src/main/java/com/produce365/care/Care.java
+++ src/main/java/com/produce365/care/Care.java
@@ -4,10 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
 @Setter
 @Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "CARE")
 public class Care {
