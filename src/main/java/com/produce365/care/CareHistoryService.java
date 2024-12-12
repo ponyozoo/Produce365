2726b96ea9d7dfb74886ff5001b9e1f36bc54c42
박호영 <hoyoung@baghoyeong-ui-MacBookAir.local>
2024-12-12 17:17:37 +0900

    [FEAT] : Care, CareHistory 관련 메소드 및 기능 구현 완료

--- src/main/java/com/produce365/care/CareHistoryService.java
+++ src/main/java/com/produce365/care/CareHistoryService.java
@@ -1,21 +1,38 @@
 package com.produce365.care;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@RequestMapping("/CareHistory")
 public class CareHistoryService {
 	
 	private final CareHistoryRepository careHistoryRepository;
 	
-	@PostMapping("/")
-	String insert(CareHistory careHistory) {
-		careHistoryRepository.save(careHistory);
-		return "";
+	public List<CareHistory>getCareHistories(){
+		return careHistoryRepository.findAll(Sort.by(Sort.Direction.DESC, "date"));
+		
+	}
+	
+	public boolean deleteCareHistories(int id) {
+		try {
+			careHistoryRepository.deleteById(id);
+			return true;
+		} catch(IllegalArgumentException e) {
+			return false;
+		}
+	}
+	
+	public boolean createCareHistory(CareHistory careHistory) {
+		try {
+			careHistoryRepository.save(careHistory);
+			return true;
+		} catch(IllegalArgumentException e) {
+			return false;
+		}
 	}
 }
