95f7d949c6a1d95c2aa6ed82f589c515c77f91ee
박호영 <hoyoung@baghoyeong-ui-MacBookAir.local>
2024-12-12 11:38:35 +0900

    [FEAT] : 컨트롤러, 서비스, HTML코드 수정 완료 CSS, JS 파일 작성 완료

--- src/main/java/com/produce365/care/CareController.java
+++ src/main/java/com/produce365/care/CareController.java
@@ -4,43 +4,47 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/care")
 public class CareController {
 	
 	private final CareService careService;
 	
-	@GetMapping("/care")
-	public String careForm(Model model) {
-	    Care care = new Care();
-	    model.addAttribute("care", care);
-	    return "pages/care";
-	}
-	
-	@PostMapping("/care")
-    public String saveCare(@ModelAttribute Care care) {
-        careService.saveCare(care);
-        return "pages/care";
+	@PostMapping("/save")
+    public String saveCare(Care care, BindingResult bindingResult, Model model) {
+		boolean result =  careService.saveCare(care);
+        if(result) 
+        	return "redirect:/care";
+    else {
+    }	model.addAttribute("msg","저장실패");
+    	return "pages/care";
     }
-
-	@GetMapping("/careList")
+	
+	@GetMapping(value = {"/", ""}) //뭔지 물어보기
     public String getAllCares(Model model) {
         List<Care> cares = careService.getCares();
         model.addAttribute("cares", cares);
-        return "pages/careList";
+        model.addAttribute("care", new Care()); //왜 한번더 했는지 물어보기
+        return "pages/care";
     }
-	@GetMapping("/cares/delete")
-	public String deleteCare(@RequestParam("id") int id) {
-		careService.deleteCare(id);
-		
-		return "pages/careList";
+	@GetMapping("/delete")
+	public String deleteCare(@RequestParam("id") int id, Model model) {
+		boolean result = careService.deleteCare(id);
+		if(result)
+			return "redirect:/care";
+		else {
+			model.addAttribute("msg", "삭제실패");
+			return "pages/care";
+		}
 		
 	}
 	
